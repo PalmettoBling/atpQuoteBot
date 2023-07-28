d809c735a2a1e18caea3bf920ce99d2331b419d3
Charles Merrick Moss <merrickm@live.com>
2023-07-28 08:57:37 -0400

    Adding comments, cleaning up some code

--- bsQuoteTimeFunction/index.js
+++ bsQuoteTimeFunction/index.js
@@ -12,44 +12,46 @@ module.exports = async function (context, myTimer) {
 
     const database = cosmosClient.database(databaseName);
     const container = database.container(containerName);
-    
-    let min = 0;
+
+    // Query number of documents in database
     let quoteCount;
-    let resource; 
     let querySpec = {
         query: 'SELECT VALUE COUNT(1) FROM c'
     };
     let { resources } = await container.items.query(querySpec, {enableCrossPartitionQuery: true}).fetchAll().catch((error) => { Error(`Completed with error ${JSON.stringify(error)}`) });
-    context.log("Resource: ");
-    
+    // verify there are documents in the database
     if (!resources) {
         quoteCount = 0;
     } else {
         quoteCount = resources[0];
     }
-
-    let randomNumInLimit = Math.floor(Math.random() * (quoteCount - min + 1)) + min;
-    context.log("Random Number? " + randomNumInLimit);
-    
+    //Generate random number from number of documents
+    let randomNumInLimit = Math.floor(Math.random() * (quoteCount - 0 + 1)) + 0;
+    // query the random document id generated
     querySpec = {
         query: `SELECT * FROM c WHERE c.id = '${randomNumInLimit}'`
     };
     let quoteData = await container.items.query(querySpec, {enableCrossPartitionQuery: true}).fetchAll().catch((error) => { Error(`Completed with error ${JSON.stringify(error)}`) });
     quoteData = quoteData.resources[0];
-    context.log(JSON.stringify(quoteData));
-    
+
+    // establishing BlueSky agent via atproto using environment secrets
     const agent = new BskyAgent({service: "https://bsky.social"});
     await agent.login({
         "identifier": process.env.ACCTID,
         "password": process.env.ACCTCRED,
     });
+    // text formatting quote data from query
     let text = quoteData.id + `: ` + quoteData.quote + ` -` + quoteData.attribution + ` / ` + quoteData.game;
 
+    // posting text to agent
     await agent.post( {text} )
     
+    //documenting run
     if (myTimer.isPastDue)
     {
         context.log('JavaScript is running late!');
+        context.log("This quote didn't run: ");
+        context.log(text);
     }
-    context.log('JavaScript timer trigger function ran!', timeStamp);   
+    context.log("This quote ran: " + text + " at ", timeStamp);   
 };
