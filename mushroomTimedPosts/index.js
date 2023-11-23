6edd792e63b4240f67a8cc9699e08906b412a51d
Charles Merrick Moss <merrickm@live.com>
2023-11-23 15:42:27 -0500

    Fixed issue with pexels library authorization

--- mushroomTimedPosts/index.js
+++ mushroomTimedPosts/index.js
@@ -5,7 +5,7 @@ module.exports = async function (context, myTimer) {
     var timeStamp = new Date().toISOString();
 
     // establishing Pexels agent via atproto using environment secrets
-    const client = createClient(`process.env.PEXELSTOKEN`);
+    const client = createClient(`${process.env.PEXELSTOKEN}`);
     const query = 'wild mushrooms';
     let chosenMushroom;
     
